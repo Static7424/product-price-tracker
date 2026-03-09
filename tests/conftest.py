523dc40e4840799be29b70c9286d53bc46103649
Static7424 <kasra.rox@gmail.com>
2026-03-09 19:10:35 +1100

    fix: Set better env vars for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,6 @@ from src.main import app
 
 
 os.environ["ENVIRONMENT"] = "test"
-os.environ["POSTGRES_VERSION"] = "17"
-os.environ["POSTGRES_PORT"] = "5432"
-os.environ["POSTGRES_DATABASE"] = "pricetracker"
-os.environ["POSTGRES_USER"] = "user"
-os.environ["POSTGRES_PASSWORD"] = "password"
-os.environ["POSTGRES_URL"] = "localhost:5432"
 
 
 @pytest.fixture
